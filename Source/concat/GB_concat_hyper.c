21e51cd8a094407f5c3343c376070985ea72ff16
Tim Davis <DrTimothyAldenDavis@gmail.com>
2024-12-16 06:57:14 -0600

    concat_hyper factory method

--- Source/concat/GB_concat_hyper.c
+++ Source/concat/GB_concat_hyper.c
@@ -132,90 +132,21 @@ GrB_Info GB_concat_hyper            // concatenate into a hypersparse matrix
             // extract the tuples from tile A
             //------------------------------------------------------------------
 
-            // if A is iso but C is not, extractTuples expands A->x [0] into
-            // all Wx [...].   If both A and C are iso, then all tiles are iso,
-            // and Wx is not extracted.
-
             int64_t anz = GB_nnz (A) ;
-            if (Ci_is_32)
-            { 
-                GB_OK (GB_extractTuples (
-                    ((csc ? Wi32 : Wj32) + pC),
-                    ((csc ? Wj32 : Wi32) + pC),
-                    (C_iso) ? NULL : (Wx + pC * csize),
-                    (uint64_t *) (&anz), ctype, A, true, Werk)) ;
-            }
-            else
-            { 
-                GB_OK (GB_extractTuples (
-                    ((csc ? Wi64 : Wj64) + pC),
-                    ((csc ? Wj64 : Wi64) + pC),
-                    (C_iso) ? NULL : (Wx + pC * csize),
-                    (uint64_t *) (&anz), ctype, A, false, Werk)) ;
-            }
-
-            //------------------------------------------------------------------
-            // adjust the indices to reflect their new place in C
-            //------------------------------------------------------------------
-
             int nth = GB_nthreads (anz, chunk, nthreads_max) ;
             int64_t pA ;
 
             if (Ci_is_32)
-            { 
-                if (cistart > 0 && cvstart > 0)
-                { 
-                    #pragma omp parallel for num_threads(nth) schedule(static)
-                    for (pA = 0 ; pA < anz ; pA++)
-                    {
-                        Wi32 [pC + pA] += cistart ;
-                        Wj32 [pC + pA] += cvstart ;
-                    }
-                }
-                else if (cistart > 0)
-                { 
-                    #pragma omp parallel for num_threads(nth) schedule(static)
-                    for (pA = 0 ; pA < anz ; pA++)
-                    {
-                        Wi32 [pC + pA] += cistart ;
-                    }
-                }
-                else if (cvstart > 0)
-                { 
-                    #pragma omp parallel for num_threads(nth) schedule(static)
-                    for (pA = 0 ; pA < anz ; pA++)
-                    {
-                        Wj32 [pC + pA] += cvstart ;
-                    }
-                }
+            {
+                #define WORK_I Wi32
+                #define WORK_J Wj32
+                #include "concat/factory/GB_concat_hyper_template.c"
             }
             else
             {
-                if (cistart > 0 && cvstart > 0)
-                { 
-                    #pragma omp parallel for num_threads(nth) schedule(static)
-                    for (pA = 0 ; pA < anz ; pA++)
-                    {
-                        Wi64 [pC + pA] += cistart ;
-                        Wj64 [pC + pA] += cvstart ;
-                    }
-                }
-                else if (cistart > 0)
-                { 
-                    #pragma omp parallel for num_threads(nth) schedule(static)
-                    for (pA = 0 ; pA < anz ; pA++)
-                    {
-                        Wi64 [pC + pA] += cistart ;
-                    }
-                }
-                else if (cvstart > 0)
-                { 
-                    #pragma omp parallel for num_threads(nth) schedule(static)
-                    for (pA = 0 ; pA < anz ; pA++)
-                    {
-                        Wj64 [pC + pA] += cvstart ;
-                    }
-                }
+                #define WORK_I Wi64
+                #define WORK_J Wj64
+                #include "concat/factory/GB_concat_hyper_template.c"
             }
 
             //------------------------------------------------------------------
---
+++ Source/concat/factory/GB_concat_hyper_template.c
@@ -0,0 +1,55 @@
+//------------------------------------------------------------------------------
+// GB_concat_hyper_template: create tuples from one tile
+//------------------------------------------------------------------------------
+
+// SuiteSparse:GraphBLAS, Timothy A. Davis, (c) 2017-2025, All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+//------------------------------------------------------------------------------
+
+// DONE: 32/64 bit
+
+// WORK_I and WORK_J are macro arguments, either uint32_t or uint64_t arrays.
+
+{
+    // if A is iso but C is not, extractTuples expands A->x [0] into
+    // all Wx [...].   If both A and C are iso, then all tiles are iso,
+    // and Wx is not extracted.
+
+    GB_OK (GB_extractTuples (
+        ((csc ? WORK_I : WORK_J) + pC),
+        ((csc ? WORK_J : WORK_I) + pC),
+        (C_iso) ? NULL : (Wx + pC * csize),
+        (uint64_t *) (&anz), ctype, A, Ci_is_32, Werk)) ;
+
+    // adjust the indices to reflect their new place in C
+
+    if (cistart > 0 && cvstart > 0)
+    { 
+        #pragma omp parallel for num_threads(nth) schedule(static)
+        for (pA = 0 ; pA < anz ; pA++)
+        {
+            WORK_I [pC + pA] += cistart ;
+            WORK_J [pC + pA] += cvstart ;
+        }
+    }
+    else if (cistart > 0)
+    { 
+        #pragma omp parallel for num_threads(nth) schedule(static)
+        for (pA = 0 ; pA < anz ; pA++)
+        {
+            WORK_I [pC + pA] += cistart ;
+        }
+    }
+    else if (cvstart > 0)
+    { 
+        #pragma omp parallel for num_threads(nth) schedule(static)
+        for (pA = 0 ; pA < anz ; pA++)
+        {
+            WORK_J [pC + pA] += cvstart ;
+        }
+    }
+}
+
+#undef WORK_I
+#undef WORK_J
